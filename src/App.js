afbba050a03b3e01e8d6629538c8596033d975d4
Андрей Спиней <enjoyhillol@gmail.com>
2024-01-14 18:37:16 +1000

    add new components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import "./styles/App.css";
 import PostForm from "./components/PostForm";
+import "./styles/App.css";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
 
   const [posts, setPosts] = useState([
-    {id: 1, title: "JavaScript", body: "Description"},
-    {id: 2, title: "JavaScript 2", body: "Description"},
-    {id: 3, title: "JavaScript 3", body: "Description"},
+    {id: 1, title: "ааа", body: "бб"},
+    {id: 2, title: "гг 2", body: "аа"},
+    {id: 3, title: "вв 3", body: "яя"},
   ])
+
+  const [selectedSort, setSelectedSort] = useState('');
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -22,11 +23,38 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost}/>
-      <PostList remove={removePost} posts={posts} title="Список постов JS"/>
+      <hr style={{margin: '15px'}} />
+      <div>
+
+          <input type="text" />
+          <MySelect
+            value={selectedSort}
+            onChange={sortPosts}
+            defaultValue="Сортировка"
+            option={[
+              {value: 'title', name: 'По названию'},
+              {value: 'body', name: 'По описанию'}
+            ]}
+          />
+      </div>
+      {posts.length
+        ? 
+        <PostList remove={removePost} posts={posts} title="Список постов JS"/>
+        : 
+        <h1 style={{textAlign: 'center'}}>
+          Посты не найдены!
+        </h1>
+      }
     </div>
+    
   );
 }
 
